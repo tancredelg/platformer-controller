2cc9374af864540a043fde195c2e7c669de4f3ea
tancredelg <tancredelamortdegail@gmail.com>
2025-02-04 20:27:18 -0500

    Added a grappler + expanded sandbox map

--- Assets/Scripts/PlatformerCharacterController.cs
+++ Assets/Scripts/PlatformerCharacterController.cs
@@ -6,11 +6,6 @@ using UnityEngine.Serialization;
 
 public class PlatformerCharacterController : MonoBehaviour
 {
-    [Header("Grounding")]
-    [SerializeField] private LayerMask GroundLayer;
-    [SerializeField][Range(0.05f, 0.5f)] private float GroundCheckCircleRadius = 0.2f;
-    [SerializeField] private Transform[] GroundChecks;
-    
     [Header("Movement")]
     [SerializeField] private float JumpSpeed = 20;
     [SerializeField] private float GroundedMoveSpeed = 20;
@@ -18,17 +13,24 @@ public class PlatformerCharacterController : MonoBehaviour
     [SerializeField] private float TerminalSpeed = 40;
     [SerializeField][Range(0, 1)] private float GroundedMoveSnapiness = 0.4f;
     [SerializeField][Range(0, 1)] private float UngroundedMoveSnapiness = 0.04f;
-    [SerializeField] private Vector2 LandingImpulseMultiplier = new Vector2(0.05f, 0.05f);
-    [SerializeField][Range(0, 3)] private int LandingImpulseToUse = 0;
-    // [SerializeField] private Vector2 ImpulseOnLanding = new Vector2(50, 0);
+    [SerializeField] private float LandingImpulseMultiplier = 0.04f;
     
+    [Header("Grounding")]
+    [SerializeField] private LayerMask GroundLayer;
+    [SerializeField][Range(0.05f, 0.5f)] private float GroundCheckCircleRadius = 0.2f;
+    [SerializeField] private Transform[] GroundChecks;
+
+    [Header("Holding & Carrying")]
+    [SerializeField] private Transform PrimaryHold;
+
     // Constants
     private const float MaxJumpQueueTime = 0.1f;
 
     // Cached references
     private Rigidbody2D rb;
-    private InputAction moveAction;
+    private InputAction moveAction, pointAction;
     private Vector2 prevLinearVelocity;
+    private Grappler primaryHoldItem;
     
     // States, counters, etc.
     private bool isGrounded, wasGrounded;
@@ -41,8 +43,22 @@ public class PlatformerCharacterController : MonoBehaviour
 
     private void Start()
     {
-        moveAction = InputSystem.actions.FindAction("Move");
         InputSystem.actions.FindAction("Jump").performed += ctx => Jump();
+        moveAction = InputSystem.actions.FindAction("Move");
+        pointAction = InputSystem.actions.FindAction("Point");
+        InputSystem.actions.FindAction("Primary Attack").performed += ctx =>
+        {
+            if (primaryHoldItem == null) return;
+            Vector2 aimPoint = Camera.main.ScreenToWorldPoint(pointAction.ReadValue<Vector2>());
+            primaryHoldItem.Fire(aimPoint);
+        };
+
+        if (PrimaryHold.childCount > 0) SetPrimaryHoldItem(PrimaryHold.GetChild(0).GetComponent<Grappler>());
+    }
+
+    private void Update()
+    {
+        Aim();
     }
 
     private void FixedUpdate()
@@ -51,23 +67,14 @@ public class PlatformerCharacterController : MonoBehaviour
         {
             wasGrounded = isGrounded;
             isGrounded = GroundChecks.Any(t => Physics2D.OverlapCircle(t.position, GroundCheckCircleRadius, GroundLayer));
-            if (wasGrounded != isGrounded) print("Now " + (isGrounded ? "grounded" : "not grounded"));
         
-            if (!wasGrounded && isGrounded)
+            // if (wasGrounded != isGrounded) print("Now " + (isGrounded ? "grounded" : "not grounded"));
+        
+            if (!wasGrounded && isGrounded)  // Just landed
             {
                 // To fix the 'stickiness' of landings, add an impulse relative to the previous x-velocity
-                var landingImpulses = new float[]
-                {
-                    prevLinearVelocity.x * Mathf.Abs(prevLinearVelocity.y) * LandingImpulseMultiplier.x,
-                    prevLinearVelocity.x * LandingImpulseMultiplier.x + Mathf.Sign(prevLinearVelocity.x) * Mathf.Abs(prevLinearVelocity.y) * LandingImpulseMultiplier.y,
-                    prevLinearVelocity.magnitude * prevLinearVelocity.x * LandingImpulseMultiplier.x,
-                    prevLinearVelocity.magnitude * Mathf.Sign(prevLinearVelocity.x) * Mathf.Abs(prevLinearVelocity.y) * LandingImpulseMultiplier.x
-                };
-                print($"x * |y| * m = {prevLinearVelocity.x} * {Mathf.Abs(prevLinearVelocity.y)} * {LandingImpulseMultiplier.x} = {landingImpulses[0]}");
-                print($"x * m.x + sign(x) * |y| * m.y = {prevLinearVelocity.x} * {LandingImpulseMultiplier.x} + {Mathf.Sign(prevLinearVelocity.x)} *{Mathf.Abs(prevLinearVelocity.y)} * {LandingImpulseMultiplier.y} = {landingImpulses[1]}");
-                print($"mag * x * m = {prevLinearVelocity.magnitude} * {prevLinearVelocity.x} * {LandingImpulseMultiplier.x} = {landingImpulses[2]}");
-                print($"mag * sign(x) * |y| * m = {prevLinearVelocity.magnitude} * {Mathf.Sign(prevLinearVelocity.x)} * {Mathf.Abs(prevLinearVelocity.y)} * {LandingImpulseMultiplier.x} = {landingImpulses[3]}");
-                rb.AddForceX(landingImpulses[LandingImpulseToUse], ForceMode2D.Impulse);
+                var landingImpulse = prevLinearVelocity.x * Mathf.Abs(prevLinearVelocity.y) * LandingImpulseMultiplier;
+                rb.AddForceX(landingImpulse, ForceMode2D.Impulse);
 
                 // Handle queued jump
                 if (Time.time < lastJumpTime + MaxJumpQueueTime) Jump();
@@ -80,13 +87,15 @@ public class PlatformerCharacterController : MonoBehaviour
 
     private void Jump()
     {
-        // rb.AddForce(Vector2.up * JumpStrength * 2, ForceMode2D.Impulse);
         if (isGrounded)
         {
             rb.linearVelocityY = JumpSpeed * 2;
         }
         else
         {
+            if (primaryHoldItem) primaryHoldItem.Cancel();
+            
+            // Queue the jump by recording the time it was attempted
             lastJumpTime = Time.time;
         }
     }
@@ -110,11 +119,20 @@ public class PlatformerCharacterController : MonoBehaviour
             // Clamp ungrounded speed
             rb.linearVelocity = Vector2.ClampMagnitude(rb.linearVelocity, TerminalSpeed);
         }
+    }
 
-        if (rb.linearVelocity.sqrMagnitude > 0.1f)
-        {
-            Debug.DrawLine(transform.position, transform.position + (Vector3) rb.linearVelocity / 4, Color.red, Time.fixedDeltaTime);
-        }
+    private void Aim()
+    {
+        var pointInput = pointAction.ReadValue<Vector2>();
+        var delta = Camera.main.ScreenToWorldPoint(pointInput) - PrimaryHold.position;
+        var lookAngle = Mathf.Atan2(delta.y, delta.x) * Mathf.Rad2Deg;
+        PrimaryHold.localEulerAngles = new Vector3(0, 0, lookAngle);
+    }
+
+    private void SetPrimaryHoldItem(Grappler item)
+    {
+        item.transform.SetParent(PrimaryHold);
+        primaryHoldItem = item;
     }
 
     private void OnDrawGizmos()
